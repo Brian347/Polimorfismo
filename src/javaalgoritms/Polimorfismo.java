137cb24c111e0639aca861b0a31d807ebd34195b
BRIAN <BRIAN@DESKTOP-FKUAPU1>
2019-09-02 17:24:02 -0500

    Cambio_6

---
+++ src/javaalgoritms/Polimorfismo.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaalgoritms;
+
+/**
+ *
+ * @author BRIAN
+ */
+/**
+ *
+ * @author BRIAN
+ */
+public class Polimorfismo {
+    
+    //CREANDO TRES METODOS sumar()
+    
+    int sumar(int a, int b){
+        return a+b;
+    }//Retorna valor de tipo entero
+    
+    double sumar(double a, double b){
+        return a+b;
+    }//Retorna un valor de tipo double
+    
+    String sumar(String a, String b){
+        return a+b;
+    }//Retorna un valor de tipo cadena
+    
+    //Los métodos se diferencian de los tipos de datos
+    //que se reciben como parámetro y los de retorno del método
+    
+}
+
