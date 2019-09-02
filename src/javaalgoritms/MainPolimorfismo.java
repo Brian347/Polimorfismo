8aaff820a95060be0dcfc73611b7a90eb580dc0d
BRIAN <BRIAN@DESKTOP-FKUAPU1>
2019-09-02 17:25:36 -0500

    Cambio_7

---
+++ src/javaalgoritms/MainPolimorfismo.java
@@ -0,0 +1,14 @@
+
+package javaalgoritms;
+/*
+ * @author BRIAN
+ */
+public class MainPolimorfismo {
+    
+    public static void main (String[] args){
+        Polimorfismo aux=new Polimorfismo();
+        System.out.println("Suma de Enteros: "+aux.sumar(12, 14));
+        System.out.println("Suma de Doubles: "+aux.sumar(33.4, 3.7));
+        System.out.println("Suma de Cadenas: "+aux.sumar("Concatena", " Palabras"));
+    }
+}
